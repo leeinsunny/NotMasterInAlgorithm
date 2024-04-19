a7946140ce98989b7b1766b0022fc0686d148f7d
이지현 <cher22ish22@gmail.com>
2024-04-19 15:03:53 +0900

    ...
    ...

---
+++ BOJ/Week4_DP/1_9095_1,2,3더하기_64%/jihyeon.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+arr = [0] * 11
+arr[1], arr[2], arr[3] = 1, 2, 4
+for i in range(4, 11):
+    arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+
+# 테스트 케이스 처리
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    print(arr[N])
